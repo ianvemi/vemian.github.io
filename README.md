80567d7f6bfffbf7218bc63c4815b42f3930f87e
ian vm <ianvemi@outlook.com>
2022-10-25 01:04:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vemian.github.io
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+    var Scrollbar = window.Scrollbar;
+
+    const options = {
+        'damping' : 0.05,
+        'renderByPixels' : true
+  
+    }
+    Scrollbar.init(document.querySelector('body'), options);
+    
+
